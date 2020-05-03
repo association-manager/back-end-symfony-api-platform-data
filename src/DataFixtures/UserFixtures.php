28eacaa0f7fe1c14808909ef14343a4a274040a2
alihasana <hasana.ali@gmail.com>
2020-05-03 14:38:47 +0200

    creation of user fixture

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+
+
+class UserFixtures extends BaseFixture
+{
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(User::class, 10, function(User $user, $count){
+            $user->setFirstName($this->faker->firstName())
+                ->setLastName($this->faker->lastName)
+                ->setEmail($this->faker->email.$count)
+                ->setCreatedAt($this->faker->dateTime)
+                ->setMobile($this->faker->phoneNumber)
+                ->setPassword('password')
+                ->setDataUsageAgreement($this->faker->randomElement([1,0]))
+            ;
+        });
+        $manager->flush();
+    }
+
+}
