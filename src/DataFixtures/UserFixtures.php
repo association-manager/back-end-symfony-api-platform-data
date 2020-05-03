bbf165f39445fa28fadb6ff53bc7b67c3bc7fe6b
alihasana <hasana.ali@gmail.com>
2020-05-03 17:22:48 +0200

    create default test user in fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -12,16 +12,15 @@ class UserFixtures extends BaseFixture
     {
         $this->createMany(User::class, 10, function(User $user, $count){
             $hash = $this->encoder->encodePassword($user, "password");
-            $user->setFirstName($this->faker->firstName())
-                ->setLastName($this->faker->lastName)
-                ->setEmail($this->faker->email.$count)
-                ->setCreatedAt($this->faker->dateTime)
-                ->setMobile($this->faker->phoneNumber)
-                ->setSex($this->faker->randomElement(['male','female', '']))
-                ->setDob($this->faker->dateTime)
-                ->setPassword($hash)
-                ->setDataUsageAgreement($this->faker->randomElement([1,0]))
-            ;
+                $user->setFirstName($this->faker->firstName())
+                    ->setLastName($this->faker->lastName)
+                    ->setEmail($count != 0 ? $this->faker->email.$count : 'test@test.com')
+                    ->setCreatedAt($this->faker->dateTime)
+                    ->setMobile($this->faker->phoneNumber)
+                    ->setSex($this->faker->randomElement(['male','female', '']))
+                    ->setDob($this->faker->dateTime)
+                    ->setPassword($hash)
+                    ->setDataUsageAgreement($this->faker->randomElement([1,0]));
         });
         $manager->flush();
     }
