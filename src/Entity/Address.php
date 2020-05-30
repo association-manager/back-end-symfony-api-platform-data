efc26a9f51e7317fcd8eec9167b5dc07078663a6
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-05-30 20:16:54 +0200

    Activation of the API platform for Address and WorkGroup

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\AddressRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=AddressRepository::class)
+ * @ApiResource
  */
 class Address
 {
--- src/Entity/WorkGroup.php
+++ src/Entity/WorkGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\WorkGroupRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=WorkGroupRepository::class)
  * @ORM\Table(name="`work_group`")
+ * @ApiResource
  */
 class WorkGroup
 {
