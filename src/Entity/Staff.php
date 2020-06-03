8377b95b2bdd45f9c417de154e10c190bbb9f94b
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-06-03 19:28:45 +0200

    Staff Entity: Configuration of the API, addition of the serialization system with the management of normalizationcontext groups, configuration of the routing system.

--- src/Entity/Staff.php
+++ src/Entity/Staff.php
@@ -2,15 +2,31 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\StaffRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\StaffRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
  * @ORM\Entity(repositoryClass=StaffRepository::class)
+ * @ApiResource(
+ *      collectionOperations={
+ *          "GET"={"path"="/personnels/lister"},
+ *          "POST"={"path"="/personnels/creer"}
+ *           },
+ *      itemOperations={
+ *          "GET"={"path"="/personnel/{id}/afficher"}, 
+ *          "PUT"={"path"="/personnel/{id}/modifier"},
+ *          "DELETE"={"path"="/personnel/{id}/supprimer"}
+ *          },
+ *      normalizationContext={
+ *          "groups"={
+ *              "staff_read"
+ *          }
+ *      }
+ * )
  */
 class Staff
 {
@@ -18,16 +34,34 @@ class Staff
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({
+     *      "staff_read", 
+     *      "member_read", 
+     *      "association_read",
+     *      "members_subresource"
+     * })
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({
+     *      "staff_read", 
+     *      "member_read", 
+     *      "association_read",
+     *      "members_subresource"
+     * })
      */
     private $name;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups({
+     *      "staff_read", 
+     *      "member_read", 
+     *      "association_read",
+     *      "members_subresource"
+     * })
      */
     private $description;
 
@@ -39,16 +73,30 @@ class Staff
 
     /**
      * @ORM\Column(type="string", length=45)
+     * @Groups({
+     *      "member_read", 
+     *      "association_read",
+     *      "members_subresource"
+     * })
      */
     private $associationType;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @ORM\Column(type="string", nullable=true, length=20)
+     * @Groups({
+     *      "staff_read", 
+     *      "member_read", 
+     *      "association_read",
+     *      "members_subresource"
+     * })
      */
     private $phoneNumber;
 
     /**
      * @ORM\ManyToMany(targetEntity=Member::class, mappedBy="staff")
+     * @Groups({
+     *      "staff_read"
+     * })
      */
     private $members;
 
@@ -110,12 +158,12 @@ class Staff
         return $this;
     }
 
-    public function getPhoneNumber(): ?int
+    public function getPhoneNumber(): ?string
     {
         return $this->phoneNumber;
     }
 
-    public function setPhoneNumber(?int $phoneNumber): self
+    public function setPhoneNumber(?string $phoneNumber): self
     {
         $this->phoneNumber = $phoneNumber;
 
