9b5c731b461ac485efdc40a701a2c177a4e6e1e4
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-06-03 19:23:01 +0200

    MemberTasWorkGroupRelation Entity: Configuration of the API, addition of the serialization system with the management of normalizationcontext groups.

--- src/Entity/MemberTaskWorkGroupRelation.php
+++ src/Entity/MemberTaskWorkGroupRelation.php
@@ -2,11 +2,29 @@
 
 namespace App\Entity;
 
-use App\Repository\MemberTaskWorkGroupRelationRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Repository\MemberTaskWorkGroupRelationRepository;
 
 /**
  * @ORM\Entity(repositoryClass=MemberTaskWorkGroupRelationRepository::class)
+ * * @ApiResource(
+ *      collectionOperations={
+ *          "GET",
+ *          "POST"
+ *           },
+ *      itemOperations={
+ *          "GET", 
+ *          "PUT",
+ *          "DELETE"
+ *          },
+ *      normalizationContext={
+ *          "groups"={
+ *              "member_task_work_group_relation_read"
+ *          }
+ *      }
+ * )
  */
 class MemberTaskWorkGroupRelation
 {
@@ -14,24 +32,52 @@ class MemberTaskWorkGroupRelation
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({
+     *      "member_task_work_group_relation_read", 
+     *      "member_read", 
+     *      "project_read", 
+     *      "project_planning_read", 
+     *      "task_read", 
+     *      "work_group_read",
+     *      "projects_subresource"
+     * })
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity=Member::class, inversedBy="memberTaskWorkGroupRelations")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({
+     *      "member_task_work_group_relation_read", 
+     *      "project_read", 
+     *      "project_planning_read", 
+     *      "task_read", 
+     *      "work_group_read",
+     *      "projects_subresource"
+     * })
      */
     private $member;
 
     /**
      * @ORM\ManyToOne(targetEntity=Task::class, inversedBy="memberTaskWorkGroupRelations")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({
+     *      "member_task_work_group_relation_read", 
+     *      "member_read"
+     * })
      */
     private $task;
 
     /**
      * @ORM\ManyToOne(targetEntity=WorkGroup::class, inversedBy="memberTaskWorkGroupRelations")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({
+     *      "member_task_work_group_relation_read", 
+     *      "member_read", 
+     *      "project_read", 
+     *      "project_planning_read", 
+     *      "task_read"
+     * })
      */
     private $workGroup;
 
