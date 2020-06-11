361bc877844d7d99086433243f054b47994434ae
alihasana <hasana.ali@gmail.com>
2020-06-11 16:30:34 +0200

    remove in required insert

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,9 +5,6 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
