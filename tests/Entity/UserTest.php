1fb438accd275eadffacd9133c5b738af70aa12c
alihasana <hasana.ali@gmail.com>
2020-06-11 14:11:58 +0200

    example of user test

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUser(): void
+    {
+        $user = new User();
+        $user->setFirstName('firstName')
+            ->setEmail('test@test.com');
+
+        $this->assertEquals('firstName', $user->getFirstName());
+        $this->assertEquals('test@test.com', $user->getEmail());
+    }
+
+}
