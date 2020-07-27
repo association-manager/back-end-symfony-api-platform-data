84aeb4bbe5f8b375c029730b192250cf85d1b24d
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-07-27 09:45:57 +0200

    alert timeout

--- public/assets/js/_alert.js
+++ public/assets/js/_alert.js
@@ -1,5 +1,5 @@
 window.setTimeout(function() {
-    $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+    $(".alert").fadeTo(4000, 0).slideUp(4000, function(){
         $(this).remove(); 
     });
 }, 4000);
