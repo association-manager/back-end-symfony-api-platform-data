e18a15ae185b2d0cd575b130dac8c7732aaa0781
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-07-28 18:03:38 +0200

    Security Update for Dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -19,95 +19,100 @@ class DashboardController extends AbstractController
      */
     public function index(AdvertisementRepository $adRepo, EntityManagerInterface $manager, StatsService $statsService)
     {
-        // ---Start admin user stats
-        $stats = $statsService->getStats();
-
-        // dd($stats);
-
-        // Ads valid staus
-        $findAllValidAds = $adRepo->findBy(array('status' => 'Validé'));
-
-        $AllValidAds = count($findAllValidAds);
-
-        // Ads Waiting for validation
-        $findAllWattingForValidationAds = $adRepo->findBy(array('status' => 'En attente de validation'));
-
-        $AllWattingForValidationAds = count($findAllWattingForValidationAds);
-
-        // Ads Validated and published
-        $findAllValidatedAndPublishedAds = $adRepo->findBy(array('status' => 'Validé et publié'));
-
-        $AllValidatedAndPublishedAds = count($findAllValidatedAndPublishedAds);
-
-        // Unpublished
-        $findAllUnpublishedAds = $adRepo->findBy(array('status' => 'Dépublié'));
-
-        $AllUnpublishedAds = count($findAllUnpublishedAds);
-
-        // Refuse
-        $findAllRefuseAds = $adRepo->findBy(array('status' => 'Refusé'));
-
-        $AllRefuseAds = count($findAllRefuseAds);
-
-        // ---End admin user stats
-        
-        //-----------------------------------
-
-                
-        // ---Start App user stats
         $user = $this->getUser();
 
-        // Ads
-        $findUserAds = $adRepo->findBy(array('user' => $user));
+        if (in_array($this->isGranted('ROLE_ADMIN'), $user->getRoles()) || in_array($this->isGranted('ROLE_ADVERTISER'), $user->getRoles())) {
+            // ---Start admin user stats
+            $stats = $statsService->getStats();
 
-        $advertisementsByAppUser = count($findUserAds);
-        // dd($advertisementsByAppUser);
-        
-        // Ads valid staus
-        $findValidAds = $adRepo->findBy(array('user' => $user, 'status' => 'Validé'));
+            // dd($stats);
 
-        $validAds = count($findValidAds);
+            // Ads valid staus
+            $findAllValidAds = $adRepo->findBy(array('status' => 'Validé'));
 
-        // Ads Waiting for validation
-        $findWattingForValidationAds = $adRepo->findBy(array('user' => $user, 'status' => 'En attente de validation'));
+            $AllValidAds = count($findAllValidAds);
 
-        $wattingForValidationAds = count($findWattingForValidationAds);
+            // Ads Waiting for validation
+            $findAllWattingForValidationAds = $adRepo->findBy(array('status' => 'En attente de validation'));
 
-        // Ads Validated and published
-        $findValidatedAndPublishedAds = $adRepo->findBy(array('user' => $user, 'status' => 'Validé et publié'));
+            $AllWattingForValidationAds = count($findAllWattingForValidationAds);
 
-        $validatedAndPublishedAds = count($findValidatedAndPublishedAds);
+            // Ads Validated and published
+            $findAllValidatedAndPublishedAds = $adRepo->findBy(array('status' => 'Validé et publié'));
 
-        // Unpublished
-        $findUnpublishedAds = $adRepo->findBy(array('user' => $user, 'status' => 'Dépublié'));
+            $AllValidatedAndPublishedAds = count($findAllValidatedAndPublishedAds);
 
-        $unpublishedAds = count($findUnpublishedAds);
+            // Unpublished
+            $findAllUnpublishedAds = $adRepo->findBy(array('status' => 'Dépublié'));
 
-        // Refuse
-        $findRefuseAds = $adRepo->findBy(array('user' => $user, 'status' => 'Refusé'));
+            $AllUnpublishedAds = count($findAllUnpublishedAds);
 
-        $refuseAds = count($findRefuseAds);
+            // Refuse
+            $findAllRefuseAds = $adRepo->findBy(array('status' => 'Refusé'));
 
-        // ---End app user stat
-        
-        // --------------------------------
+            $AllRefuseAds = count($findAllRefuseAds);
 
-        return $this->render('ad_admin/dashboard/home.html.twig', [
-            // Admin stats
-            'stats'     => $stats,
-            'AllValidAds' => $AllValidAds,
-            'AllWattingForValidationAds' => $AllWattingForValidationAds,
-            'AllValidatedAndPublishedAds' => $AllValidatedAndPublishedAds,
-            'AllUnpublishedAds' => $AllUnpublishedAds,
-            'AllRefuseAds' => $AllRefuseAds,
+            // ---End admin user stats
+            
+            //-----------------------------------
 
-            // User stats
-            'advertisementsByAppUser' => $advertisementsByAppUser,
-            'validAds' => $validAds,
-            'wattingForValidationAds' => $wattingForValidationAds,
-            'validatedAndPublishedAds' => $validatedAndPublishedAds,
-            'unpublishedAds' => $unpublishedAds,
-            'refuseAds' => $refuseAds
-        ]);
+                    
+            // ---Start App user stats
+            $user = $this->getUser();
+
+            // Ads
+            $findUserAds = $adRepo->findBy(array('user' => $user));
+
+            $advertisementsByAppUser = count($findUserAds);
+            // dd($advertisementsByAppUser);
+            
+            // Ads valid staus
+            $findValidAds = $adRepo->findBy(array('user' => $user, 'status' => 'Validé'));
+
+            $validAds = count($findValidAds);
+
+            // Ads Waiting for validation
+            $findWattingForValidationAds = $adRepo->findBy(array('user' => $user, 'status' => 'En attente de validation'));
+
+            $wattingForValidationAds = count($findWattingForValidationAds);
+
+            // Ads Validated and published
+            $findValidatedAndPublishedAds = $adRepo->findBy(array('user' => $user, 'status' => 'Validé et publié'));
+
+            $validatedAndPublishedAds = count($findValidatedAndPublishedAds);
+
+            // Unpublished
+            $findUnpublishedAds = $adRepo->findBy(array('user' => $user, 'status' => 'Dépublié'));
+
+            $unpublishedAds = count($findUnpublishedAds);
+
+            // Refuse
+            $findRefuseAds = $adRepo->findBy(array('user' => $user, 'status' => 'Refusé'));
+
+            $refuseAds = count($findRefuseAds);
+
+            // ---End app user stat
+            
+            // --------------------------------
+
+            return $this->render('ad_admin/dashboard/home.html.twig', [
+                // Admin stats
+                'stats'     => $stats,
+                'AllValidAds' => $AllValidAds,
+                'AllWattingForValidationAds' => $AllWattingForValidationAds,
+                'AllValidatedAndPublishedAds' => $AllValidatedAndPublishedAds,
+                'AllUnpublishedAds' => $AllUnpublishedAds,
+                'AllRefuseAds' => $AllRefuseAds,
+
+                // User stats
+                'advertisementsByAppUser' => $advertisementsByAppUser,
+                'validAds' => $validAds,
+                'wattingForValidationAds' => $wattingForValidationAds,
+                'validatedAndPublishedAds' => $validatedAndPublishedAds,
+                'unpublishedAds' => $unpublishedAds,
+                'refuseAds' => $refuseAds
+            ]);
+
+        }
     }
 }
