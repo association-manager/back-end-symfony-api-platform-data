b1e07cee750fd09f9f8db696594a7ca7f3b9ba00
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-08-28 17:32:20 +0200

    Set custom data in jwt token

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,35 @@
+<?php
+
+    namespace App\EventListener;
+    use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+    use Symfony\Component\Security\Core\User\UserInterface;
+
+    class AuthenticationSuccessListener{
+    
+        /**
+         * @param AuthenticationSuccessEvent $event
+         * @return void
+         */
+        public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+        {
+            $data = $event->getData();
+            $user = $event->getUser();
+            $addresses = $event->getUser()->getAddresses();
+
+            if (!$user instanceof UserInterface) {
+                return;
+            }
+        
+        
+            $data['data'] = array(
+                'id' => $user->getId(),
+                'firstName' => $user->getFirstName(),
+                'lastName' => $user->getLastName(),
+                'addresse' => $addresses[0]->getId()
+            );
+        
+            $event->setData($data);
+        }
+    }
+?>
